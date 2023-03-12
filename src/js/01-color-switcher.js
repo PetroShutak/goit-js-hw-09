35ab748eb3a444a3ac03f95b763f0f8c3873a426
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-03-13 00:28:27 +0200

    add color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,31 @@
-// Напиши скрипт, який після натискання кнопки «Start», раз на секунду змінює колір фону <body> 
-// на випадкове значення, використовуючи інлайн стиль. 
-// Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
-
-// УВАГА
-// Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів.
-//  Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
-
-// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
-
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 // }
+
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+};
+
+refs.btnStart.addEventListener('click', onStartBtnClick);
+refs.btnStop.addEventListener('click', onStopBtnClick);
+
+let timerId = null;
+
+function onStartBtnClick() {
+    refs.btnStart.disabled = true;
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onStopBtnClick() {
+    clearInterval(timerId);
+    refs.btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
