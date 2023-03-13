2d9886f3626eee69dbda6dc53294996a67291878
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-03-13 23:48:16 +0200

    promises: second try

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,56 +1,38 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
         resolve({ position, delay });
-      }, delay);
-    });
-  } else {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
+      } else {
         reject({ position, delay });
-      }, delay);
-    });
-  }
-}
-
-const refs = {
-  form: document.querySelector('.form'),
-  input: document.querySelector('input[name="amount"]'),
-  step: document.querySelector('input[name="step"]'),
-  btn: document.querySelector('button[type="submit"]'),
-};
-
-refs.btn.addEventListener('click', onBtnClick);
-
-function onBtnClick(event) {
-  event.preventDefault();
-  const amount = refs.input.value;
-  const step = refs.step.value;
-  const promises = [];
-  let delay = 0;
-
-  for (let i = 1; i <= amount; i += 1) {
-    delay += step;
-    createPromise(i, delay)
-    .then(({ position, delay }) => {
-      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
-  }
-}
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
   });
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const step = Number(form.elements.step.value);
+  const amount = Number(form.elements.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    const position = i + 1;
+    const currentDelay = delay + step * i;
+
+    createPromise(position, currentDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
