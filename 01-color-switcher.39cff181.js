be98e6c05fbace56e4193be6b576c83f7ef9f6d5
PetroShutak <113899587+PetroShutak@users.noreply.github.com>
2023-03-14 17:47:56 +0000

    Deploying to gh-pages from @ PetroShutak/goit-js-hw-09@c5a61930259408b6778668522f5031d61e2b7011 🚀

--- 01-color-switcher.39cff181.js
+++
@@ -1,2 +0,0 @@
-const t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.btnStop.addEventListener("click",(function(){clearInterval(e),t.btnStart.disabled=!1}));let e=null;
-//# sourceMappingURL=01-color-switcher.39cff181.js.map
